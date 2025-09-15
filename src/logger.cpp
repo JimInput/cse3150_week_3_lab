57fdb1f6806fc2ef01526c1990c8dcb2e11610fd
JimInput <oJimmy05o@gmail.com>
2025-09-15 10:22:28 -0400

    completed lab

--- src/logger.cpp
+++ src/logger.cpp
@@ -8,27 +8,67 @@ void addReading(const std::string& label, double value,
                 std::string labels[], double values[],
                 int& size, int capacity) {
     // TODO: throw "Full" if size == capacity, else insert and ++size
+   if (size == capacity) {
+        throw std::string("Full");
+   }
+
+   labels[size] = label;
+   values[size++] = value;
 }
 
 void updateValue(double* valuePtr, double newValue) {
     // TODO: write through pointer
+    if (valuePtr == nullptr) {
+        return;
+    }
+
+    *valuePtr = newValue;
 }
 
 void printReading(const std::string& label, const double& value) {
     // TODO: pretty-print one reading
+    std::cout << label << ": " << value << std::endl;
 }
 
 double average(const double values[], int size) {
     // TODO: throw "Empty" if size==0, else compute average
-    return 0.0;
+    if (size == 0) {
+        throw std::string("Empty");
+    }
+    double sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += values[i];
+    }
+    return sum / size;
 }
 
 double minValue(const double values[], int size) {
     // TODO: throw "Empty" if size==0, else compute min
-    return 0.0;
+    if (size == 0) {
+        throw std::string("Empty");
+    }
+
+    double min = values[0];
+    for (int i = 1; i < size; i++) {
+        if (values[i] < min) {
+            min = values[i];
+        }
+    }
+    return min;
 }
 
 double maxValue(const double values[], int size) {
     // TODO: throw "Empty" if size==0, else compute max
-    return 0.0;
+    if (size == 0) {
+        throw std::string("Empty");
+    }
+
+    double max = values[0];
+    for (int i = 1; i < size; i++) {
+        if (values[i] > max) {
+            max = values[i];
+        }
+    }
+    return max;
 }
